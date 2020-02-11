0acab63d381146cf4162a5ff00c0235373294c07
Lima Martins <d_mart04@WIWI.UNI-MUENSTER.DE>
2020-02-11 15:42:28 +0100

    Correcting notebooks and adding ISM experimental case

--- experiments/queryreverseengineering/adultqueries.py
+++
@@ -1,8 +0,0 @@
-queries = [
-    'age >= 19 and age <= 22 and education == " Bachelors"',
-    'occupation == " Armed-Forces"',
-    'nativecountry == " Philippines" and age >= 30 and age <= 40',
-    'workclass == " Private" and race ==" Asian-Pac-Islander"',
-    'capitalgain > 9999'
-    #,'maritalstatus == " Divorced" and workclass == " State-gov" and age > 70'
-]
